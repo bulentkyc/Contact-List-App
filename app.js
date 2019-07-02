8de42071764b29334e5dbafa5a3dce9140f17333
bulentkyc <bulent.kayici@digitalcareerinstitute.org>
2019-07-02 14:59:23 +0200

    l5

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 const router = require('./controller/router');
 
 app.set('views', __dirname + '/view');
